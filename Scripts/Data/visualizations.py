ddc60ed8cad554028f8ce27db7422e0dc7be946a
Josh <joshfisher81@gmail.com>
2024-06-02 23:03:59 -0400

    plot city detectors

---
+++ Scripts/Data/visualizations.py
@@ -0,0 +1,30 @@
+import plotly.express as px
+import pandas as pd
+
+from Scripts.constants import utd, cities_d
+
+def plot_city_detectors(city: str, dark=False):
+    # cities = set(utd.keys())
+    # if city not in cities:
+    #     raise ValueError(f"{city} is not in the dataset")
+
+    utd_city = utd.filter_city(city)
+    df_traff = utd_city.traffic_df
+    df_detec = utd_city.detector_df
+
+    df_traff = df_traff.groupby('detid')[['flow']].mean().reset_index()
+    df = df_traff.merge(df_detec, on='detid', how='outer')
+    df = df.dropna()
+
+    mapbox_stype = "carto-darkmatter" if dark else "carto-positron"
+    city_data = cities_d[city]
+    lat, lon = city_data['latitude'], city_data['longitude']
+
+    fig = px.scatter_mapbox(
+        df, lat='lat', lon='long', mapbox_style=mapbox_stype, color='flow', size='flow', zoom=13,
+        center={'lat': lat, 'lon': lon}
+    )
+    fig.show()
+
+if __name__ == "__main__":
+    plot_city_detectors('paris')
