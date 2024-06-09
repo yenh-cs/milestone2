7acc1503edcf9f781320e4bd01ffc5a96ceeba97
Josh <joshfisher81@gmail.com>
2024-06-09 13:50:46 -0400

    added links to map plot

--- Scripts/Data/visualizations.py
+++ Scripts/Data/visualizations.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objs as go
 import seaborn as sns
 import numpy as np
 from tqdm import tqdm
@@ -8,11 +9,15 @@ from Scripts.constants import utd
 from Scripts.cache_viz import cache_plot
 
 @cache_plot
-def plot_city_detectors(city: str, dark=False):
+def plot_city_detectors(city: str, dark=False, links=False):
     assert city in utd.cities, f"{city} not in UTD"
-    utd_city = utd.get_city_dfs(city, link_flag=False)
+    if links:
+        utd_city = utd.get_city_dfs(city)
+    else:
+        utd_city = utd.get_city_dfs(city, True, True, False)
     df_traff = utd_city.traffic_df
     df_detec = utd_city.detector_df
+    df_link = utd_city.link_df
 
     df_traff = df_traff.groupby('detid')[['flow']].mean().reset_index()
     df = df_traff.merge(df_detec, on='detid', how='outer')
@@ -24,8 +29,22 @@ def plot_city_detectors(city: str, dark=False):
 
     fig = px.scatter_mapbox(
         df, lat='lat', lon='long', mapbox_style=mapbox_style, color='flow', size='flow', zoom=12,
-        center={'lat': lat, 'lon': lon}
+        center={'lat': lat, 'lon': lon}, hover_name='detid'
     )
+    if links:
+        add_trace = go.Scattermapbox(
+            lat=df_link['lat'],
+            lon=df_link['long'],
+            mode='markers',
+            marker=go.scattermapbox.Marker(
+                color='lightsalmon'
+            ),
+            customdata=df_link[['linkid', 'order']].values,
+            hovertemplate="<b>linkid:</b> %{customdata[0]}<br>" +
+                            "<b>order:</b> %{customdata[1]}<br>"
+        )
+
+        fig.add_trace(add_trace)
     return fig
 
 @cache_plot
@@ -53,5 +72,5 @@ def plot_flow_distibutions(n_cols=3):
 if __name__ == "__main__":
     # res = plot_flow_distibutions(n_cols=4)
     # plt.show()
-    fig = plot_city_detectors('london', dark=False)
+    fig = plot_city_detectors('paris', dark=False, links=True)
     fig.show()
